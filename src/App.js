0a2a7f12a217de94a0c61bc62bbdc1883582c553
Cat Sync <catsync@zdh.com>
2020-06-06 14:39:19 -0400

    Basic drag drop machine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Machine, assign } from 'xstate'
+import { useMachine } from '@xstate/react'
 
-function App() {
+const dragDropMachine = Machine(
+  {
+    id: 'dragDropMachine',
+    initial: 'idle',
+    context: {
+      // element position
+      x: 0,
+      y: 0,
+      // where you clicked relative to the target element
+      offsetX: 0,
+      offsetY: 0,
+    },
+    states: {
+      idle: {
+        on: {
+          mousedown: {
+            target: 'dragging',
+            actions: ['startDrag'],
+          },
+        },
+      },
+      dragging: {
+        on: {
+          mouseup: {
+            target: 'idle',
+          },
+          mousemove: {
+            target: 'dragging',
+            internal: true,
+            actions: ['updateDrag'],
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      startDrag: assign((ctx, mouseEvent) => {
+        const { x, y } = mouseEvent.target.getBoundingClientRect()
+        return {
+          ...ctx,
+          x,
+          y,
+          offsetX: mouseEvent.clientX - x,
+          offsetY: mouseEvent.clientY - y,
+        }
+      }),
+      updateDrag: assign((ctx, mouseEvent) => {
+        return {
+          ...ctx,
+          x: mouseEvent.clientX - ctx.offsetX,
+          y: mouseEvent.clientY - ctx.offsetY,
+        }
+      }),
+    },
+  }
+)
+
+export default function App() {
+  const [state, send] = useMachine(dragDropMachine, {
+    actions: {
+      log: (c, e) => console.log('****', e.type),
+    },
+  })
+
+  const handleMouse = React.useCallback(e => {
+    send(e)
+  },[send])
+
+  React.useEffect(() => {
+    document.addEventListener('mouseup', handleMouse)
+    document.addEventListener('mousemove', handleMouse)
+    return () => {
+      document.removeEventListener('mouseup', handleMouse)
+      document.removeEventListener('mousemove', handleMouse)
+    }
+  },[handleMouse])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          position: 'absolute',
+          height: '150px',
+          width: '150px',
+          backgroundColor: 'purple',
+          borderRadius: '50%',
+          top: state.context.y,
+          left: state.context.x,
+        }}
+        onMouseDown={handleMouse}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+        }}
+      >
+        {state.value}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
